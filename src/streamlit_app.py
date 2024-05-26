288613dcedc9993d95812d328f78eb4f4e712ae9
jethro_low <jethro_low@aiap.sg>
2024-05-27 03:11:44 +0800

    update code to use llama index with recursive and reranking

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 from chatbot import *
+from streamlit_chat import message as st_message
 
 def main():
 
@@ -25,45 +26,32 @@ def main():
         st.session_state.history_model = []
     if "source" not in st.session_state:
         st.session_state.source = []
-    if "is_finance_report" not in st.session_state:
-        st.session_state.is_finance_report = ""
     if "final_store" not in st.session_state:
         st.session_state.final_store = None
+    if "input_text" not in st.session_state:
+        st.session_state.input_text = None
 
     st.markdown("<h1 style='text-align: center; color: white;'>Financial Reports Q&A Chatbot!</h1>", unsafe_allow_html=True)
 
-    col1, col2 = st.columns(2)
-    with col1:
+    st.header("Chat")
+    
+    with st.sidebar:
+        st.subheader("Upload your financial statements")
+        pdf_file = st.file_uploader("Upload a financial statement document. Please ensure file is in PDF format.", accept_multiple_files = False)
 
-        st.header("Chat")
-        
-        with st.sidebar:
-            st.subheader("Upload your financial statements")
-            fr_pdf_docs = st.file_uploader("You can upload more than one financial statement documents. Please ensure file is in PDF format.", accept_multiple_files = True)
+        if st.button("OK"):
+            with st.spinner("Reading your document..."):
+                # preprocessing
+                st.session_state.final_store = preprocessing(pdf_file= pdf_file)
+                if st.session_state.final_store is not None:
+                    st.write("Ready! You can start chatting with your financial reports!")
 
-            if st.button("OK"):
-                with st.spinner("Reading your document..."):
-                    # preprocessing
-                    final_store = preprocessing(fr_pdf_docs= fr_pdf_docs)
-                    if final_store is not None:
-                        st.write("Ready! You can start chatting with your financial reports!")
-            if st.session_state.is_finance_report is not None:
-                st.write(st.session_state.is_finance_report)
+    # generate answers
+    st.text_input("Ask me a question relating to the financial statements", key = "input_text", on_change = generate_answer)
 
-        # generate answers
-        st.text_input("Ask me a question relating to the financial statements", key = "input_text", on_change = generate_answer)
-
-        #display chats
-        for chat in st.session_state.history:
-            st_message(**chat)
-
-    # show source documents
-    with col2:
-        st.header("Source")
-        st.write("""This is the source document for the latest response from the ChatBot.
-        Inspect the source document to ensure factual truth of the response""")
-        if st.session_state.source:
-            st.write(st.session_state.source)
+    #display chats
+    for chat in st.session_state.history:
+        st_message(**chat)
 
 if __name__ == '__main__':
     main()
